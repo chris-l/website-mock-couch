fb759f8eedb0c90d37e8298ff7a1025d9c9afc42
Christopher Luna <dev@christopher-luna.com>
2014-05-07 01:39:32 -0500

    Smooth scroll added

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 /* jslint node: true */
-/*global document */
+/*global document, window */
 'use strict';
 require('modernizr');
 require('prism');
 var $ = require('jquery');
 
 require('bootstrap');
-$(document).ready(function() {
+$(window.document).ready(function() {
   $('#sidebar').affix({
     offset: {
       top: 430
@@ -22,3 +22,22 @@ $(document).ready(function() {
   });
 });
 
+$('a').click(function(){
+  var href = $.attr(this, 'href'),
+      anchor = $('[name="' + $.attr(this, 'href').substr(1) + '"]');
+
+  // Do animation if the anchor exists
+  if(anchor.length > 0) {
+    $('html, body').animate({
+      scrollTop: anchor.offset().top - 60
+    }, 500,  function () {
+      window.location.hash = href;
+    });
+    return false;
+  }
+
+  // Do the default action if not
+  if(anchor.length === 0) {
+    return true;
+  }
+});
