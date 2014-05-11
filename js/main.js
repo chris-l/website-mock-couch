cae154003249387e62adbd1fc6e846f5dc0dc60b
Christopher Luna <dev@christopher-luna.com>
2014-05-11 12:49:03 -0500

    Clean up the JavaScript code

--- js/main.js
+++ js/main.js
@@ -1,35 +1,32 @@
 /* jslint node: true */
-/*global document, window */
+/*global window */
 'use strict';
+var $ = require('jquery');
 require('modernizr');
 require('prism');
-var $ = require('jquery');
-
 require('bootstrap');
-$(window.document).ready(function() {
-  $('#sidebar').affix({
-    offset: {
-      top: 430
-    }
-  });
 
-  var $body   = $(document.body);
-  var navHeight = $('.navbar').outerHeight(true) + 10;
-
-  $body.scrollspy({
-    target: '#leftCol',
-    offset: navHeight
-  });
+$('#sidebar').affix({
+  offset: {
+    top: 430
+  }
 });
 
-$('a').click(function(){
-  var href = $.attr(this, 'href'),
-      anchor = $('[name="' + $.attr(this, 'href').substr(1) + '"]');
+var navHeight = $('#header').outerHeight(true) + 10;
+
+$('body').scrollspy({
+  target: '#leftCol',
+  offset: navHeight
+});
+
+$('a').click(function() {
+  var href   = $.attr(this, 'href'),
+      anchor = $($.attr(this, 'href'));
 
   // Do animation if the anchor exists
   if(anchor.length > 0) {
     $('html, body').animate({
-      scrollTop: anchor.offset().top - 60
+      scrollTop: anchor.offset().top - 0
     }, 500,  function () {
       window.location.hash = href;
     });
